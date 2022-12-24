d8fc722e1bda2672b0db35d0bf5a98efb3cdad70
Romain Ares <argolys@outlook.fr>
2022-12-24 02:50:40 +0100

    Adding dist

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'https://argozen1.github.io/Rocket-league-e-commerce/',
-  plugins: [react()],
-})
